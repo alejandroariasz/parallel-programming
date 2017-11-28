6c5190dd2c0ecc90da884ba8755d86e3b3e9dce6
alejandroariasz <alejandro.arias022@gmail.com>
2017-11-27 16:04:19 -0800

    Fix partitioning posix

--- huaariasza-dabaquerot/posix/blur-effect.cpp
+++ huaariasza-dabaquerot/posix/blur-effect.cpp
@@ -209,8 +209,7 @@ int main(int argc, char** argv)
 		//variable to add half of the kernel in each side of the block
 		int kernelDiff = (kernel_size - 1) / 2;
 
-		//variables to store edges of the block adding half of the kernel in each side
-		int startRect = 0;
+		//variable to store edges of the block adding half of the kernel
 		int endRect = 0;
 
 		for (int i = 0; i < input.rows; i = i + input.rows / blocks)
@@ -222,20 +221,17 @@ int main(int argc, char** argv)
                 		blockheigth = ceil(input.rows / blocks);
 
             		//if block is not at the beggining add half of the kernel
-			if(i > 0) startRect = i - kernelDiff;
+			if(i > 0) i = i - kernelDiff;
 
 			endRect = blockheigth + kernelDiff;
 
 			//new rect of the block + kernel size
-			cv::Rect rect = cv::Rect(0, startRect, input.cols, endRect);
+			cv::Rect rect = cv::Rect(0, i, input.cols, endRect);
 
 			//add block to block images
 			blockImages.push_back(cv::Mat(input, rect));
         	}
 	}
-
-	//variable to store start time
-	int start_s = clock();
 
 	//Region threads
 
@@ -272,6 +268,9 @@ int main(int argc, char** argv)
 			exit(-1);
 		}
 	}
+
+	//variable to store start time
+	int start_s = clock();
 
 	//vector to store the generated blocks
 	vector < Mat > bluredImages;
